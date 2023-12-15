85614d3d8455b397168ef747351095658a0d93bc
Felicia <Felicia@FeliHP>
2023-12-15 17:00:35 +0700

    nothing

--- src/Try.java
+++ src/Try.java
@@ -17,7 +17,9 @@ public class Try {
         int term;
         int st_goal = 0;
         int st_choice;
-        int yes=0;
+        int yes = 0;
+        int exit_app = 0;
+        int categories_choice;
 
         while (true) {
             System.out.println("Budgeting App");
@@ -26,18 +28,20 @@ public class Try {
             System.out.println("1. Goals");
             System.out.println("2. Categories");
             System.out.println("3. Listing");
+            System.out.println("4. Exit app");
             System.out.println("==============");
-            System.out.println("Where would you like to go? (1-3)");
+            System.out.println("Where would you like to go? (1-4)");
             menu = s.nextInt();
-            while (menu > 3 || menu < 1) {
-                System.out.println("Input not valid, please input 1-3");
+            while (menu > 4 || menu < 1) {
+                System.out.println("Input not valid, please input 1-4");
                 menu = s.nextInt();
             }
 
             switch (menu) {
+
                 case 1:
                     System.out.println("What term goals would you like to set");
-                    System.out.println("1. Short term goal");3
+                    System.out.println("1. Short term goal");
                     System.out.println("2. Medium term goal");
                     System.out.println("3. Long term goal");
                     term = s.nextInt();
@@ -47,33 +51,61 @@ public class Try {
                     }
                     switch (term) {
                         case 1:
-                            while(yes<1){
-                            System.out.println("set goal : " + st_goal + "/month");
-                            System.out.println("1. Set goal");
-                            System.out.println("2. Edit goal");
-                            System.out.println("3. Exit menu");
-                            System.out.print("choose : ");
-                            st_choice = s.nextInt();
-                            while (st_choice > 3 || st_choice < 1) {
-                                System.out.println("Input is not valid, please input 1-2");
+                            while (yes < 1) {
+                                System.out.println("set goal : " + st_goal + "/month");
+                                System.out.println("1. Set goal");
+                                System.out.println("2. Edit goal");
+                                System.out.println("3. Exit menu");
+                                System.out.print("choose : ");
                                 st_choice = s.nextInt();
-                            }
+                                while (st_choice > 3 || st_choice < 1) {
+                                    System.out.println("Input is not valid, please input 1-2");
+                                    st_choice = s.nextInt();
+                                }
 
-                            switch (st_choice) {
-                                case 1:
-                                    System.out.println("Set up a short term goal!");
-                                    System.out.print("goal : ");
-                                    st_goal = s.nextInt();
-                                    break;
-                                case 2: 
-                                    break;
-                                case 3:
-                                    yes+=1;
-                                    break;
-                                    
+                                switch (st_choice) {
+                                    case 1:
+                                        System.out.println("Set up a short term goal!");
+                                        System.out.print("goal : ");
+                                        //should it be able to make another goal or just one short term goal
+                                        st_goal = s.nextInt();
+                                        break;
+                                    case 2:
+                                        System.out.println("Edit goal");
+                                        System.out.println("Current goal: " + st_goal);
+                                        System.out.print("Change goal to : ");
+                                        st_goal = s.nextInt();
+                                        break;
+                                    case 3:
+                                        yes += 1;
+                                        break;
+                                }
+
                             }
-                            }
+                        case 2:
+                            break;
                     }
+
+                case 2:
+//                            while(){
+                    System.out.println("Categories!");
+                    System.out.println("1. Add");
+                    System.out.println("2. Edit");
+                    System.out.print("choose: ");
+                    categories_choice = s.nextInt();
+
+                    switch (categories_choice) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                    }
+//                    }
+                    break;
+
+                case 4:
+                    System.exit(0);
+                    break;
             }
         }
     }
